a283b50d643e427e0c50c61edf1ca0d319adddd7
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-07-19 20:29:10 +0200

    Ratings counter

--- src/main/scala/com/sundogsoftware/spark/RatingsCounter.scala
+++ src/main/scala/com/sundogsoftware/spark/RatingsCounter.scala
@@ -11,21 +11,18 @@ object RatingsCounter extends App {
   Logger.getLogger("org").setLevel(Level.ERROR)
 
   // Create a SparkContext using every core of the local machine, named RatingsCounter
-  val sc = new SparkContext("local[*]", "RatingsCounter")
+  new SparkContext("local[*]", "RatingsCounter")
+    // Load up each line of the ratings data into an RDD
+    .textFile("data/ml-100k/u.data")
+    // Convert each line to a string, split it out by tabs, and extract the third field.
+    // (The file format is userID, movieID, rating, timestamp)
+    .map(_.split("\t")(2))
+    // Count up how many times each value (rating) occurs
+    .countByValue()
+    // Sort the resulting map of (rating, count) tuples
+    .toSeq
+    .sortBy(_._1)
+    // Print each result on its own line.
+    .foreach(println)
 
-  // Load up each line of the ratings data into an RDD
-  val lines = sc.textFile("data/ml-100k/u.data")
-
-  // Convert each line to a string, split it out by tabs, and extract the third field.
-  // (The file format is userID, movieID, rating, timestamp)
-  val ratings = lines.map(_.split("\t")(2))
-
-  // Count up how many times each value (rating) occurs
-  val results = ratings.countByValue()
-
-  // Sort the resulting map of (rating, count) tuples
-  val sortedResults = results.toSeq.sortBy(_._1)
-
-  // Print each result on its own line.
-  sortedResults.foreach(println)
 }
